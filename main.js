a5f1a5de7e921da9c052370fba442cbc125f9781
Dimitris2031 <dimitriskokranis21@gmail.com>
2025-08-17 22:08:25 +0300

    basic constructor knowledge

--- main.js
+++ main.js
@@ -1,58 +1,37 @@
-function getComputerChoice(){
-    let item;
-    let randomInt = Math.floor(Math.random() * 3) + 1;
-    if (randomInt== 1){
-        item = "paper";
-    } else if(randomInt == 2){
-        item = "rock";
-    }else{
-        item = "scissors";
+const myLibrary = [];
+
+class Book {
+    constructor(bookName,Author){
+        this.bookName = bookName;
+        this.Author = Author;
     }
-    return item;
+    talk(){
+        console.log(this.bookName,this.Author);
+    }
 }
 
-function playRound(compChoice,PlayerChoice){
+function addbook(){
+    const main =  document.querySelector('.main');
+    main.innerHTML = '';
+    event.preventDefault();
+
+    const form = event.target;
+    const {bookName,Author} = form.elements;
     
-    if (compChoice==PlayerChoice){
-        console.log("TIE!");
-        return 0;
-    }
+    myLibrary.push(new Book(bookName.value,Author.value));
+    for (const book of myLibrary){
+        const div = document.createElement('div');
+        const h1 = document.createElement('h1');
+        const h2 = document.createElement('h2');
 
-    if (compChoice == "paper" && PlayerChoice == "rock" ||
-        compChoice == "rock" && PlayerChoice == "scissors" ||
-        compChoice == "scissors" && PlayerChoice == "paper") {
-        console.log(`You Lose! ${compChoice} beats ${PlayerChoice}`);
-        return 1;
-    } else {
-        console.log(`You Win! ${PlayerChoice} beats ${compChoice}`);
-        return 2;
+        h1.textContent = `Book Name: ${book.bookName}`;
+        h2.textContent = `Author: ${book.Author}`;
+
+        div.append(h1,h2);
+        main.appendChild(div);
+
+        console.log(book['Author']);
+
     }
+    form.reset();
 }
-
-
-function game(){
-    let compScore = 0;
-    let playerScore = 0;
-
-    while (compScore < 5 && playerScore < 5) {
-        const playerSelection = prompt("choose your weapon: ");
-        computerSelection = getComputerChoice(); 
-        const rounds = playRound(computerSelection,playerSelection)
-      
-        if (rounds == 1) {
-          compScore++;
-        } else if (rounds == 2) {
-          playerScore++;
-        }
-      }
-    alert(compScore);
-    alert(playerScore);
-    if (compScore > playerScore) {
-    return("The Computer Won!");
-    } else {
-    return("You won!");
-    }
-}
-
-
-console.log(game());
